910049fe4074fc2e74f37700a6f3e212d3347576
mayatarek <Mayatarek@iCloud.com>
2024-12-27 11:43:42 +0200

    added some methods implementation

--- src/main/java/entities/BookList.java
+++ src/main/java/entities/BookList.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BookList {
     private List<Book> books;
@@ -12,29 +13,33 @@ public class BookList {
         return books;
     }
 
-
     /// /////////////////////////////////
     /// /////////////////////////////////
+    /// /////////////////////////////////
 
-    public void addBook(Book book){
-        //implement
-    }
-
-    public void removeBook(Book book){
-        //implement
-    }
 
     public boolean isEmpty(){
         return books.isEmpty();
     }
 
-//    public Book searchByName(String bookName){
-//        //implement
-//    }
-//
-//    public Book searchByID(int bookID){
-//        //implement
-//    }
+    public Book searchByID(int bookID){
+        for(Book book : books){
+            if(bookID == book.getId())
+                return book;
+        }
+        return null;
+    }
+
+    public Book searchByName(String bookName){
+        for(Book book : books){
+            //this was recommended by intelij idk if it's correct
+            if(Objects.equals(bookName, book.getTitle()))
+                return book;
+        }
+        return null;
+    }
+
+
 
 
 }
